c55a6ebd354f68f16b47bb68866ebf60f4449850
Kacper <kkacper99@o2.pl>
2022-04-03 15:47:41 +0200

    Merge branch 'master' of https://git.wmi.amu.edu.pl/s462072/WALL-E

--- main.py
+++ main.py
@@ -1,36 +1,60 @@
 import pygame
-from map import preparedMap
+from map import *
 from agent import trashmaster
 from house import House
+from sprites import *
 
 class WalleGame():
     
     def __init__(self):
-        self.SCREEN_SIZE = [512, 512]
-        self.BACKGROUND_COLOR = '#ffffff'
+        self.SCREEN_SIZE = [1024, 768]
+        #self.BACKGROUND_COLOR = '#ffffff'
         
         pygame.init()
         pygame.display.set_caption('Wall-e')
         
         
         self.screen = pygame.display.set_mode(self.SCREEN_SIZE)
-        self.screen.fill(pygame.Color(self.BACKGROUND_COLOR))
+        # self.screen.fill(pygame.Color(self.BACKGROUND_COLOR))
         
         # krata
-        self.map = preparedMap(self.SCREEN_SIZE)
-        self.screen.blit(self.map, (0,0))
-        
+        # self.map = preparedMap(self.SCREEN_SIZE)
+        # self.screen.blit(self.map, (0,0))
+        self.walls = pg.sprite.Group()
+        self.player_img = pg.image.load("./resources/textures/garbagetruck/trashmaster_blu.png").convert_alpha()
+        self.all_sprites = pg.sprite.Group()
+
+
+        self.map = TiledMap("resources/textures/map/roads.tmx")
+        self.map_img = self.map.make_map()
+        self.map_rect = self.map_img.get_rect()
+        self.screen.blit(self.map_img, (0,0))
+
+        for tile_object in self.map.tmxdata.objects:
+            if tile_object.name == 'player':
+                self.player = Player(self, tile_object.x, tile_object.y)
+            if tile_object.name == 'wall':
+                Obstacle(self, tile_object.x, tile_object.y, tile_object.width, tile_object.height)
+
+
+
+        # self.camera = Camera(self.map.width, self.map.height)
+        # self.screen.blit(self.map_img, self.camera.apply_rect(self.map_rect))
+
+
     def update_window(self):
         pygame.display.update()
+        # self.camera.update(self.image)
     
     def draw_object(self, drawable_object, pos):
         # pos => (x, y)
         # drawable object must have .image field inside class
         self.screen.blit(drawable_object.image, pos )
+        
 
     def reloadMap(self):
-        self.screen.fill(pygame.Color(self.BACKGROUND_COLOR))
-        self.screen.blit(self.map, (0,0))
+         #self.screen.fill(pygame.Color(self.BACKGROUND_COLOR))
+         self.screen.blit(self.map_img, (0,0))
         
 def main():
     game = WalleGame()
@@ -39,9 +63,9 @@ def main():
     smieciara_object = trashmaster(16,16,"./resources/textures/garbagetruck/trashmaster_blu.png")
     game.draw_object(smieciara_object, (0, 0))
 
-    house_object = House(20, 20)
+    #house_object = House(20, 20)
     # Test draw house object
-    game.draw_object(house_object, (20,20))
+    #game.draw_object(house_object, (20,20))
 
     game.update_window()
 
@@ -53,8 +77,7 @@ def main():
                 running = False
             if event.type == pygame.KEYDOWN:
                 game.reloadMap()
-                game.draw_object(smieciara_object, 
-                                        smieciara_object.movement(event.key, 16))
+                game.draw_object(smieciara_object, smieciara_object.movement(event.key, 16))
                 
                 game.update_window()
 
--- map.py
+++ map.py
@@ -1,13 +1,65 @@
-import pygame
+import pygame as pg
+import pytmx
 
 # config
-TILE_SIZE = 16
+# TILE_SIZE = 16
 
-def preparedMap(screenSize):
-    tileImage = pygame.image.load('tile1.png')
-    surface = pygame.Surface(screenSize)
+# def preparedMap(screenSize):
+#     tileImage = pg.image.load('tile1.png')
+#     surface = pg.Surface(screenSize)
 
-    for x in range(0, screenSize[0], TILE_SIZE):
-        for y in range(0, screenSize[1], TILE_SIZE):
-            surface.blit(tileImage, (x, y))
-    return surface
+#     for x in range(0, screenSize[0], TILE_SIZE):
+#         for y in range(0, screenSize[1], TILE_SIZE):
+#             surface.blit(tileImage, (x, y))
+#     return surface
+def collide_hit_rect(one, two):
+    return one.hit_rect.colliderect(two.rect)
+class TiledMap:
+    #loading file
+    def __init__(self, filename):
+        tm = pytmx.load_pygame(filename, pixelalpha=True)
+        self.width = tm.width * tm.tilewidth
+        self.height = tm.height * tm.tileheight
+        self.tmxdata = tm
+    
+    #rendering map
+    def render(self, surface):
+        ti = self.tmxdata.get_tile_image_by_gid
+        for layer in self.tmxdata.visible_layers:
+            if isinstance(layer, pytmx.TiledTileLayer):
+                for x, y, gid, in layer:
+                    tile = ti(gid)
+                    if tile:
+                        surface.blit(tile, (x * self.tmxdata.tilewidth, y * self.tmxdata.tilewidth))
+    
+    def make_map(self):
+        temp_surface = pg.Surface((self.width, self.height))
+        self.render(temp_surface)
+        return temp_surface
+
+class Camera:
+    def __init__(self,width,height):
+        self.camera = pg.Rect(0,0, width, height)
+        self.width = width
+        self.height = height
+
+    def apply(self,entity):
+        return entity.rect.move(self.camera.topleft)
+
+    # def apply_rect(self, rect):
+    #     return rect.move(self.camera.topleft)
+    
+    def update(self,target):
+        x = -target.rect.x + int(1024/2)
+        y = -target.rect.y + int(768 / 2)
+
+        # limit scrolling to map size
+        x = min(0, x)  # left
+        y = min(0, y)  # top
+        x = max(-(self.width - 1024), x)  # right
+        y = max(-(self.height - 768), y)  # bottom
+        self.camera = pg.Rect(x, y, self.width, self.height)
+
+
+
+
