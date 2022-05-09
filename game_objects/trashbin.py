1971d4104dc0fa68637c2a2e6f49e63a6455b722
czorekk <bartek.wieczorek@outlook.com>
2022-05-10 01:05:34 +0200

    [trashbin-rendering] added trash bin rendering, added patterns, sprite group and part in map generator

--- game_objects/trashbin.py
+++ game_objects/trashbin.py
@@ -1,29 +1,23 @@
 import pygame as pg
+from enum import Enum
 
-class trashbin(pg.sprite.Sprite):
-    
-    def __init__(self,x,y,img, type):
-        super().__init__()
+from map.tile import Tile
+
+class Waste_Type(Enum):
+    BIO = 0
+    GLASS = 1
+    PLASTIC = 2
+    PAPER = 3
+    MIX = 4
+
+    def __int__(self):
+        return self.value
+class Trashbin(Tile):
+    def __init__(self, img, x, y, width, height, waste_type: Waste_Type):
+        super().__init__(img, x, y, width, height)
         
-        # trashbin type
-        self.type = type
-
-        # dimensions
-        if type == "small":
-            self.width = 4
-            self.height = 4
-        elif type == "medium":
-            self.width = 8
-            self.height = 8
-        elif type == "large":
-            self.width = 16
-            self.height = 16
-            
-        # spawn coords
-        self.x = x
-        self.y = y
-    
-        # load trashbin image
-        self.image = pg.image.load(img)
-        self.image = pg.transform.scale(self.image, (self.x,self.y))
-        self.rect = self.image.get_rect()
+        self.waste_type = waste_type
+        self.days_after_pickup = 0
+        self.max_capacity = 100
+        self.used_capacity = 0
+        self.access = True
