b1d2d04448bcf42be0d18e10f9d9e78fbf79a871
Kacper <kkacper99@o2.pl>
2022-06-10 02:12:10 +0200

    tsp v3

--- genetic_algorithm/TSP.py
+++ genetic_algorithm/TSP.py
@@ -183,10 +183,10 @@ def geneticAlgorithm(population, popSize, eliteSize, mutationRate, generations):
 
 cityList = []
 
-for i in range(0,25):
-    cityList.append(City(x=int(random.random() * 200), y=int(random.random() * 200)))
+# for i in range(0,25):
+#     cityList.append(City(x=int(random.random() * 200), y=int(random.random() * 200)))
 
-geneticAlgorithm(population=cityList, popSize=100, eliteSize=20, mutationRate=0.01, generations=1000)
+# geneticAlgorithm(population=cityList, popSize=100, eliteSize=20, mutationRate=0.01, generations=1000)
 
 
 
@@ -197,15 +197,20 @@ def geneticAlgorithmPlot(population, popSize, eliteSize, mutationRate, generatio
     pop = initialPopulation(popSize, population)
     progress = []
     progress.append(1 / rankRoutes(pop)[0][1])
-    
+    print("Initial distance: " + str(1 / rankRoutes(pop)[0][1]))
+
     for i in range(0, generations):
         pop = nextGeneration(pop, eliteSize, mutationRate)
         progress.append(1 / rankRoutes(pop)[0][1])
     
+    print("Final distance: " + str(1 / rankRoutes(pop)[0][1]))
+    bestRouteIndex = rankRoutes(pop)[0][0]
+    bestRoute = pop[bestRouteIndex]
+
     plt.plot(progress)
     plt.ylabel('Distance')
     plt.xlabel('Generation')
     plt.show()
-
+    return bestRoute
 
 # geneticAlgorithmPlot(population=cityList, popSize=100, eliteSize=20, mutationRate=0.01, generations=1000)
